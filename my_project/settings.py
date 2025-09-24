6f3100a475d1beaf71a6badb26f20255377495a2
Rishij Manna <rishijmanna2018@gmail.com>
2025-09-25 00:09:26 +0530

    req3

--- my_project/settings.py
+++ my_project/settings.py
@@ -126,17 +126,14 @@ LOGIN_URL = '/login/'
 
 # =======================
 # =======================
-# Email (works local + render)
-# =======================
+# Email (works both local + Render)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'  # or smtp-relay.brevo.com if you use Brevo
-EMAIL_PORT = 587
+EMAIL_HOST = os.environ.get("EMAIL_HOST", "smtp-relay.brevo.com")
+EMAIL_PORT = int(os.environ.get("EMAIL_PORT", 587))
 EMAIL_USE_TLS = True
-
-# These must be set in environment (.env locally, Render → Dashboard → Environment Variables)
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL", EMAIL_HOST_USER)
 
 
 # =======================
